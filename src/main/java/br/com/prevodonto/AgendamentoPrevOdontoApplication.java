bf176eebc88dc3df099c3952e3e3533cfeef8771
Igor Oliveira <yggor_2008@hotmail.com>
2018-11-01 11:21:27 -0300

    Projeto TCC

---
+++ src/main/java/br/com/prevodonto/AgendamentoPrevOdontoApplication.java
@@ -0,0 +1,12 @@
+package br.com.prevodonto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendamentoPrevOdontoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendamentoPrevOdontoApplication.class, args);
+	}
+}
